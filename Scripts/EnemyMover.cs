ba30d31a1ee7abcec1f1988e8ec429110affbd28
AmbulantIsles <43480348+AmbulantIsles@users.noreply.github.com>
2019-03-10 23:43:42 -0400

    Add files via upload

---
+++ Scripts/EnemyMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    // animate the game object from -1 to +1 and back
+
+    public float startPoint = -1.0F;
+    public float endPoint = 1.0F;
+
+    void Update()
+    {
+        gameObject.transform.localPosition = new Vector2(Mathf.Lerp(startPoint, endPoint, Mathf.PingPong(Time.time, 1)), transform.localPosition.y);
+    }
+}
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,41 +1,161 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 	private Rigidbody2D rb2d;
 	public float speed;
     public float jumpForce;
-    
+    public Text countText;
+    public Text winText;
+    private int count;
+    public Text livesText;
+    public Text loseText;
+    public GameObject spawnPoint;
+    private int lives;
+    private bool grounded;
+    public Camera gameCamera;
+
+    Animator anim;
+    private SpriteRenderer playerSpriteRenderer;
+
     void Start()
     {
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
 		rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        loseText.text = "";
+        SetCountText();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            grounded = true;
+            anim.SetInteger("State", 0);
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.gameObject.tag == "Ground")
         {
-            if (Input.GetKey(KeyCode.UpArrow))
+
+            grounded = true;
+            anim.SetInteger("State", 0);
+            if (Input.GetKeyDown(KeyCode.UpArrow))
             {
+                grounded = false;
+                anim.SetInteger("State", 2);
                 rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) & (collision.gameObject.tag == "Ground"))
+        {
+            grounded = true;
+            anim.SetInteger("State", 1);
+            playerSpriteRenderer.flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) & (collision.gameObject.tag == "Ground"))
+        {
+            grounded = true;
+            anim.SetInteger("State", 1);
+            playerSpriteRenderer.flipX = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+            anim.SetInteger("State", 2);
         }
     }
 
     void FixedUpdate()
     {
-		float moveHorizontal = Input.GetAxis("Horizontal");
-
+        float moveHorizontal = Input.GetAxis("Horizontal");
 		Vector2 movement = new Vector2(moveHorizontal, 0);
-
-		rb2d.AddForce(movement * speed);
+		rb2d.AddForce(speed * movement);
     }
 
-    private void Update()
+    void Update()
     {
         if (Input.GetKey("escape"))
+        {
             Application.Quit();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) & (grounded == true))
+        {
+            anim.SetInteger("State", 1);
+            playerSpriteRenderer.flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) & (grounded == true))
+        {
+            anim.SetInteger("State", 1);
+            playerSpriteRenderer.flipX = false;
+        }
+
+        if (grounded == false)
+        {
+            anim.SetInteger("State", 2);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 4)
+        {
+            gameCamera.transform.position = new Vector3 (0, -12, -1);
+            gameObject.transform.position = new Vector2 (0, -12);
+            lives = 3;
+        }
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+    // sets the life counter and lose state
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            gameObject.SetActive(false);
+            loseText.text = "You Lose";
+        }
     }
 }
